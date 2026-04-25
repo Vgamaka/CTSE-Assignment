aeac802ac08d4154c9e093531103cb7b68fa39f6
Kavidu M Kulathunga <154060942+KaviduMalshan99@users.noreply.github.com>
2026-04-25 06:49:47 +0530

    Add CORS origin for Netlify app

--- app/main.py
+++ app/main.py
@@ -15,7 +15,8 @@ app = FastAPI(title="Event Service")
 
 origins = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    "https://cloudeventsystem.netlify.app"
 ]
 
 app.add_middleware(
